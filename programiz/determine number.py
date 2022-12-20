1065cc3a1b6c2b4089e95d5aecbf2bad13fa4afb
nzangi <nzangimuoki284@gmail.com>
2022-12-20 23:18:07 +0300

    Solving Quadratic equation

---
+++ programiz/determine number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter thr number to check: "))
+if number > 1:
+    print("The number is Positive")
+elif number < 0:
+    print("The number is Negative")
+else:
+    print("The number is zero")
