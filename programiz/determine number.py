078e44007dd9046dc8d54dc1de3600154ec5c85a
nzangi <nzangimuoki284@gmail.com>
2022-12-25 20:52:28 +0300

    checking a  Letter

--- programiz/determine number.py
+++ programiz/determine number.py
@@ -1,11 +1,12 @@
-letter = "o"
-if letter.upper() == "A":
-    print("The Letter is A")
-else:
-    if letter.upper() == "B":
-        print("The Letter is B")
-    else:
-        if letter.upper() == "C":
-            print("The Letter is C")
+year = int(input("Enter the year which you need to check if its is leap year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"This year {year} it is a leap year")
         else:
-            print("The Letter is not A,B and C")
+            print(f"The year {year} it is not a leap year")
+    else:
+        print(f"This year {year} it is a leap year")
+else:
+    print(f"The year {year} it is not a leap year")
