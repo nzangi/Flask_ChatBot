6a0996d3c275e8659359abf9711770f64c96b09e
nzangi <nzangimuoki284@gmail.com>
2023-01-11 18:51:03 +0300

    new updates

--- programiz/determine number.py
+++ programiz/determine number.py
@@ -30,31 +30,31 @@ for your_list in your_lists:
 # print("Twikle, twikle, twikle little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwikle ,twikle,twikle little star,\n\tHow I wonder who you are")
 
 
-# from platform import python_version
-#
-# print("The current version of python is: ", python_version())
+from platform import python_version
 
-# import datetime
-# now = datetime.datetime.now()
-# print(f'Current Datetime: {now.strftime("%Y-%m-%d %H:%M:%S")}')
-# from math import pi
-# radius = float(input("Enter the radius of the circle: "))
-# area = pi * (radius **2)
-# print(f"The area of the circle is: {area}")
-# first_name = input("Enter your first name: ")
-# last_name = input("Enter your last name: ")
-# fullname = first_name+" "+last_name
-# print(f"The revered order of name {fullname} is: {fullname[::-1]}")
-# values = input("Enter number of comma separated numbers: ")
-# list = values.split(",")
-# tuple = tuple(list)
-# print(f"List: {list}")
-# print(f"Tuple: {tuple}")
-# filename = input("Enter the Input file: ")
-# file_extension = filename.split(".")
-# print(f"The Output file is: {file_extension[-1]}")
-# color_list = ["Red","Green","White","Black"]
-# print(f"The first color is: {color_list[0]} and last color is: {color_list[-1]}")
+print("The current version of python is: ", python_version())
+
+import datetime
+now = datetime.datetime.now()
+print(f'Current Datetime: {now.strftime("%Y-%m-%d %H:%M:%S")}')
+from math import pi
+radius = float(input("Enter the radius of the circle: "))
+area = pi * (radius **2)
+print(f"The area of the circle is: {area}")
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+fullname = first_name+" "+last_name
+print(f"The revered order of name {fullname} is: {fullname[::-1]}")
+values = input("Enter number of comma separated numbers: ")
+list = values.split(",")
+tuple = tuple(list)
+print(f"List: {list}")
+print(f"Tuple: {tuple}")
+filename = input("Enter the Input file: ")
+file_extension = filename.split(".")
+print(f"The Output file is: {file_extension[-1]}")
+color_list = ["Red","Green","White","Black"]
+print(f"The first color is: {color_list[0]} and last color is: {color_list[-1]}")
 
 exam_date = (11, 12, 2015)
 print("The examination will start on: %i / %i / %i " %exam_date)
